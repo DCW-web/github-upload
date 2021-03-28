df032403c3b5cceb837953461bd3a779a4fe0936
cd <river.caiwu@gmail.com>
2021-03-28 12:29:45 -0400

    initializing repository

---
+++ build/grid_path_searcher/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cd/ws_test/src/grid_path_searcher
+# Build directory: /home/cd/ws_test/build/grid_path_searcher
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/waypoint_generator/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cd/ws_test/src/waypoint_generator
+# Build directory: /home/cd/ws_test/build/waypoint_generator
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
